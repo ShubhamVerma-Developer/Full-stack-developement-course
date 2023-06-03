2988ee271229571ecb512aedacdb08b19f6c44a6
shubham <shubham25122001@gmail.com>
2023-06-03 14:17:21 +0530

    102. Comparators and Equality

---
+++ Section 15 - Intermediate Javascript/102. Comparators and Equality.js
@@ -0,0 +1,25 @@
+==	equal to	
+x == 8	false	
+x == 5	true	
+x == "5" true
+
+===	equal value and equal type
+	x === 5	true	
+x === "5"	false
+
+!=	not equal	
+x != 8	true	
+
+!==	not equal value or not equal
+ type	x !== 5	false	
+x !== "5"	true	
+x !== 8	true	
+
+>	greater than	
+x > 8	false	
+<	less than	
+x < 8	true	
+>=	greater than or equal to	
+x >= 8	false	
+<=	less than or equal to
+	x <= 8	true
