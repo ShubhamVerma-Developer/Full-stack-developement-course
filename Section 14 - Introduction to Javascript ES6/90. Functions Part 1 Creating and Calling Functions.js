0696149e99bfde960a1717008b81ed8dc84fd763
shubham <shubham25122001@gmail.com>
2023-06-03 07:24:16 +0530

    90. Functions Part 1 Creating & Calling Functions

---
+++ Section 14 - Introduction to Javascript ES6/90. Functions Part 1 Creating and Calling Functions.js
@@ -0,0 +1,19 @@
+function getMilk(){
+    console.log("Leavehouse");
+    console.log("upward");
+    console.log("upward");
+    console.log("upward");
+    console.log("upward");
+    console.log("moveleft");
+    console.log("buymilk");
+    console.log("moveright");
+    console.log("movedown");
+    console.log("movedown");
+    console.log("movedown");
+    console.log("movedown");
+    console.log("move left");
+    console.log("reached home");
+         
+    }
+    
+    getMilk();
