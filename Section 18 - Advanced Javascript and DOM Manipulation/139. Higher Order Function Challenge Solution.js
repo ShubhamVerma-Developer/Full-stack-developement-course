b255363eef8dfbefa5b6f576ff1c6fee80d073bc
shubham <shubham25122001@gmail.com>
2023-06-05 19:53:20 +0530

    139. Higher Order Function Challenge Solution

---
+++ Section 18 - Advanced Javascript and DOM Manipulation/139. Higher Order Function Challenge Solution.js
@@ -0,0 +1,22 @@
+// Higher Order Function Challenge Solution
+
+
+function add(num1, num2) {
+return num1 + num2;
+}
+ 
+function subtract(num1, num2) {
+return num1 - num2;
+}
+ 
+function multiply(num1, num2) {
+return num1 * num2;
+}
+ 
+function divide(num1, num2) {
+return num1 / num2;
+}
+ 
+function calculator(num1, num2, operator) {
+return operator(num1, num2);
+}
