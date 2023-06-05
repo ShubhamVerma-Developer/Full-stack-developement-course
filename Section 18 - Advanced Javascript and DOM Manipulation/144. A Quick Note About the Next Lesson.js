72a591dc5831b56fa90bd14b61698156a23f40e6
shubham <shubham25122001@gmail.com>
2023-06-05 22:33:12 +0530

    144. A Quick Note About the Next Lesson

---
+++ Section 18 - Advanced Javascript and DOM Manipulation/144. A Quick Note About the Next Lesson.js
@@ -0,0 +1,8 @@
+// A Quick Note About the Next Lesson
+// Hey guys,
+
+
+
+// TL;DR keypress is now deprecated, you should use keydown instead.
+
+// Just a quick heads up, as technology moves incredibly fast, every week or so something else will change. This is just a quick reminder that in the next lesson, when we cover detecting key presses, you should be using the keydown event listener instead of keypress.
