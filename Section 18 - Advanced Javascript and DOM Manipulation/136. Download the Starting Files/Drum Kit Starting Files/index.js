71a6b4c9290e3b7aa214bf4b16b9cf17af3bd447
shubham <shubham25122001@gmail.com>
2023-06-05 14:16:03 +0530

    137. Adding Event Listeners to a Button

--- Section 18 - Advanced Javascript and DOM Manipulation/136. Download the Starting Files/Drum Kit Starting Files/index.js
+++ Section 18 - Advanced Javascript and DOM Manipulation/136. Download the Starting Files/Drum Kit Starting Files/index.js
@@ -1,12 +1,8 @@
-function soundPlay(sound){
-    var audio = new Audio('./sounds/' +sound+'.mp3');
-    audio.play();
-}
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i=0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I am clicked");
+        
 
-document.querySelectorAll(".w")[0].setAttribute("onclick", 'soundPlay("tom-1")');
-document.querySelectorAll(".a")[0].setAttribute("onclick", 'soundPlay("tom-2")');
-document.querySelectorAll(".s")[0].setAttribute("onclick", 'soundPlay("tom-3")');
-document.querySelectorAll(".d")[0].setAttribute("onclick", 'soundPlay("tom-4")');
-document.querySelectorAll(".j")[0].setAttribute("onclick", 'soundPlay("crash")');
-document.querySelectorAll(".k")[0].setAttribute("onclick", 'soundPlay("kick-bass")');
-document.querySelectorAll(".l")[0].setAttribute("onclick", 'soundPlay("snare")');
+    });
+}
