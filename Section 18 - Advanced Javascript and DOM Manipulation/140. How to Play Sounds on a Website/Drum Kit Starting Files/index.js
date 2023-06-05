b142cb9ee44b4093ee90fe1ddb56c5500069977c
shubham <shubham25122001@gmail.com>
2023-06-05 20:43:33 +0530

    140. How to Play Sounds on a Website

--- Section 18 - Advanced Javascript and DOM Manipulation/140. How to Play Sounds on a Website/Drum Kit Starting Files/index.js
+++ Section 18 - Advanced Javascript and DOM Manipulation/140. How to Play Sounds on a Website/Drum Kit Starting Files/index.js
@@ -1,6 +1,11 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
 for (var i=0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I am clicked");
+     console.log(this.style.color = "white");
     });
 }
+
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+// audio.play();
