d4564bb0a6b1cf6ab02ae87997493a02eb921df0
shubham <shubham25122001@gmail.com>
2023-06-13 09:17:07 +0530

    204. Nodemon Installation

---
+++ Section 24 - Express.js with Node.js/204. Nodemon Installation/my-express-server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send("<h1>Hello world!</h1>");
+});
+
+app.listen(3000, function(){
+    console.log("server connectd on the port : 3000");
+});
