41d3e470ee56d9165c05bd7c9a25a8a603489377
shubham <shubham25122001@gmail.com>
2023-06-15 09:02:59 +0530

    209. Responding to Requests with HTML Files

---
+++ Section 24 - Express.js with Node.js/209. Responding to Requests with HTML Files/calculator.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(){
+    console.log("Server connected successfully on port : 3000");
+});
