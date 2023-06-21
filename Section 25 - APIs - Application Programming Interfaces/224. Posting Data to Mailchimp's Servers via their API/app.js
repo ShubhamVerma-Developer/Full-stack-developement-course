87ed3fa8c07ac74afe86e1cf47573c7c34928d73
shubham <shubham25122001@gmail.com>
2023-06-21 17:38:55 +0530

    224. Posting Data to Mailchimp's Servers via their API

---
+++ Section 25 - APIs - Application Programming Interfaces/224. Posting Data to Mailchimp's Servers via their API/app.js
@@ -0,0 +1,67 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    
+const data = {
+    members: [
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_field: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }
+    ]
+}
+
+
+const jsonData = JSON.stringify(data);
+
+const url ="https://us10.api.mailchimp.com/3.0/lists/d9e6819a61"
+const options = {
+   method: "POST",
+   auth: "shubham:6f0f5bef5ff7adb93e6f327feab39e83-us10"
+
+}
+
+const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+    console.log(JSON.parse(data));
+    })
+})
+request.write(jsonData);
+request.end();
+
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
+
+
+
+
+
+// api key : 6f0f5bef5ff7adb93e6f327feab39e83-us10
+// token : d9e6819a61
