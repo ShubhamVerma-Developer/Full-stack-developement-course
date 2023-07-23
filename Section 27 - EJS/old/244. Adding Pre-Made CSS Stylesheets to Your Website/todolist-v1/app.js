fcccc0acd473742633a5854ee54060e3e6144d8d
shubham <shubham25122001@gmail.com>
2023-07-23 11:09:23 +0530

    What is EJS

---
+++ Section 27 - EJS/old/244. Adding Pre-Made CSS Stylesheets to Your Website/todolist-v1/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+   
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    // console.log(item);
+    items.push(item);
+
+    res.redirect("/");
+
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started on port : 3000");
+}); 
